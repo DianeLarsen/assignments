19d29673bf84540f4d33b87397a4912e3350bb82
Diane Larsen <diane.larsen@outlook.com>
2022-11-12 21:22:56 -0800

    finished princess

--- princess/index.js
+++ princess/index.js
@@ -1,64 +1,86 @@
 const power = ["Dead", "Small", "Big", "Powered Up"]
 
 class Player{
-    constructor(name, totalCoins, status, hasStar){
+    constructor(name, totalCoins, status, hasStar ){
     this.name = name;
-    this.totalCoins = totalCoins;
-    this.status = status;
-    this.hasStar = hasStar;
+    this.totalCoins = totalCoins || 0;
+    this.status = status || "Big";
+    this.hasStar = hasStar || false;
     }
-    
-}
-function setName(namePicked){
-    //sets name to either Mario or Luigi
-}
-function gotHit(namePicked){
-    //status will change from "Powered Up" to "Big" to "Small" to "Dead"
-}
+ }
+const user = new Player
+
 function gotPowerup(){
-    let i = power.indexOf(Player.status);
-    if(i >= 0 && i < power.length - 1){
-      let nextItem = power[i + 1]
-      return nextItem
-    }
-    
-    Player.status = nextItem
-    console.log(this.status)
-    //add i to index of 
-    //increases life and if powered up gets a start
-    if (this.status == "Powered Up"){
-        hasStar = true
+    if (user.status == "Powered Up" && user.hasStar == false){
+        user.hasStar = true
+        console.log("Congratulations!  You got a star!")
+    }else if(user.status != "Powered Up"){    
+        let i = power.indexOf(user.status);
+        let nextItem = power[i + 1]
+        user.status = nextItem
     }else{
-        hasStar = false
+        return
+        
     }
-    
+}
+function setName(){
+    let randomPlayer = Math.floor(Math.random() * 100)
+    if (randomPlayer < 50){
+        user.name = "Mario"
+    } else{
+        user.name = "Luigi"
+    }
+}
+function printIt(){
+    return `Name: ${user.name}\nStatus: ${user.status}\nTotal Coins: ${user.totalCoins}`
     
 }
+function gotHit(){
+    if (user.status == "Powered Up" && user.hasStar == true){
+        console.log("Your star protected you!")
+        user.hasStar = false
+    }else{   
+        let i = power.indexOf(user.status);
+        let prevItem = power[i - 1]
+        user.status = prevItem
+    }
+}
+
 function addCoin(){
-    //adds to total coins
+    return user.totalCoins++
 }
-function print(){
-    // Name: Luigi,
-    // Status: Small, etc, etc
-    // totalCoins: 
-    // starProperties:
-}
-
-
-
-// setInterval(() => {
-  function randomRange() {
+function randomRange() {
     return Math.floor(Math.random() * 3);
-    }
-    //0: gotHit()
-    //1: gotPowerup()
-    //2: addCoin()
-    // then print()
-//}   
-// }, interval);
+}
 
-let mario = new Player("Mario", 0 ,"Big", false)
-//console.log(mario.totalCoins)
-console.log(`Name: ${mario.name}\nStatus: ${mario.status}\nTotal Coins: ${mario.totalCoins}`)
-//console.log(mario.gotPowerup())
-console.log(mario.status)
+
+setInterval(() => {
+    if (user.name == undefined){
+        setName()
+    }
+    if (user.status == "Dead"){
+        console.log("You lost the game! You died!")
+        console.log(printIt())
+        console.log(`Your final score was ${user.totalCoins}`)
+        process.exit()
+    }
+    console.log(printIt())
+   
+    if (user.hasStar == true){
+        console.log("You have a star")
+    }
+    console.log("\n")
+    randomRange() 
+    if (randomRange()===0){
+            gotHit()
+    } else if (randomRange()===1){
+            gotPowerup()
+    } else {
+            addCoin()
+    }
+    }   
+, 1250);
+
+
+
+
