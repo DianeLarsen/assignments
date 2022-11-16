610f6db16af7bee23adfda41a35d0cc45157992c
Diane Larsen <diane.larsen@outlook.com>
2022-11-15 18:39:54 -0800

    pokemon info

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,12 +1,23 @@
 const xhr = new XMLHttpRequest()
         // method  // url                     // async?
-xhr.open("GET", "https://swapi.dev/api/people/1", true)
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
 xhr.send()
 
 xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
         const JSONdata = xhr.responseText
         const data = JSON.parse(JSONdata)
-        console.log(data)
+        console.log(data.objects[0].pokemon)
+        showData(data.objects[0].pokemon)
+    } else if (xhr.readyState === 4 && xhr.status !== 200){
+        console.log(xhr.responseText)
     }
+}
+function showData(arr){
+    for (let i = 1; i < arr.length; i++) {
+        const h1 = document.createElement('h1')
+        h1.textContent = arr[i].name
+        document.body.appendChild(h1)
+    }
+
 }
