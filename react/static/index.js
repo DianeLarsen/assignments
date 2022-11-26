56ba473fb129aab8f2a5aa5359e5d32cac710465
Diane Larsen <diane.larsen@outlook.com>
2022-11-25 20:53:59 -0800

    practice react

--- react/static/index.js
+++ react/static/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
+const page = (<nav>
+    <h1>Brand Name</h1>
+    <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+    </ul>
+    </nav>)
+const root = document.getElementById("root")
+root.append(JSON.stringify(page))
+// ReactDOM.render(
+//     page, document.getElementById("root"))
