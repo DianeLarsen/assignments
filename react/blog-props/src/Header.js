c358a93d8b786326e80d4297ee0b080519294fe2
Diane Larsen <diane.larsen@outlook.com>
2022-11-30 20:03:37 -0800

    started second project file, working on first

--- react/blog-props/src/Header.js
+++ react/blog-props/src/Header.js
@@ -3,7 +3,11 @@ import React from "react"
 export default function Header() {
     return (
         <div className= "header">
-        <img src="https://startbootstrap.github.io/startbootstrap-clean-blog/assets/img/home-bg.jpg"/>
+            <img src="https://startbootstrap.github.io/startbootstrap-clean-blog/assets/img/home-bg.jpg"/>
+            <div className="imgtext">
+                <h1>Clean Blog</h1>
+                <span className="subheading">A Blog Theme by Start Bootstrap</span>
+            </div>
         </div>
     )
 }
--- react/blog-props/src/Navbar.js
+++ react/blog-props/src/Navbar.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <a className="navbar-brand" href="index.html">Start Bootstrap</a>
             
-            <button class="navbar-toggler collapsed" type="button" > MENU <i className="fas fa-bars"></i> 
+            <button className="navbar-toggler collapsed" type="button" > MENU <i className="fas fa-bars"></i> 
             </button>
 
         </nav>
