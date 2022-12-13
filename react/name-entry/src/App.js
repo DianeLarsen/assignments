5cfa7993f4bcab857d2ad19ad7e88b0437ac7095
Diane Larsen <diane.larsen@outlook.com>
2022-12-13 11:50:35 -0800

    framework

--- react/name-entry/src/App.js
+++ react/name-entry/src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() {
+  const {name, setName} = []
+  const updateList = name.map(setName(prevName => {return {...prevName, newName}}))
+  
   return (
     <div className="App">
-      
-        
+      {/* user will type into input box */}
+      <input></input>
+      {/* {data will show up in h1} */}
+        <h1>Test: {name}</h1>
+        <button></button> 
+        {/* when button is clicked an li will apear below with the name in it and each name will be added to the list  */}
+        <ol>
+          {name}
+        </ol>
     </div>
   );
 }
