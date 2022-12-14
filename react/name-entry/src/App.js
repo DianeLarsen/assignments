d6c74c49581083d9b2edfc6e464e54465d673ed4
Diane Larsen <diane.larsen@outlook.com>
2022-12-14 08:27:46 -0800

    updated code

--- react/name-entry/src/App.js
+++ react/name-entry/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import React from "react"
+import Name from "./Name"
 
 function App() {
   return (
     <div className="App">
-      
+      <Name />
         
     </div>
   );
