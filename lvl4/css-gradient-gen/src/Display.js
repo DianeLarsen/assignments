7b1f12649e38a2b281e4256f61cf163d6d5bedc7
Diane Larsen <diane.larsen@outlook.com>
2023-01-03 16:17:48 -0800

    needs CSS

--- lvl4/css-gradient-gen/src/Display.js
+++ lvl4/css-gradient-gen/src/Display.js
@@ -1,15 +1,15 @@
 import React from "react"
 
+
 export default function Display(props){
 
-let background = `background:linear-gradient(${props.pct}deg,${props.color1}, ${props.color2})\n`+`-moz-background:linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2} )\n`+ `-webkit:linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2})`
+let background1 = `background:linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2})\n-moz-background:linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2} )\n-webkit:linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2})`
 
     return(
         <div className="display-box">
-            <div className="color-box" style={{background: `linear-gradient(${props.pct}, ${props.color1}, ${props.color2})`}}></div>
-            <textarea readOnly rows={3} value={background}></textarea> 
+            <div className="color-box" style={{background: `linear-gradient(${props.pct}deg, ${props.color1}, ${props.color2})`}}></div>
+            <textarea readOnly rows={3} value={background1}></textarea> 
         </div>
     )
 }
 
-// setInput1={setInput1} setInput2={setInput2} setPct={setPct} color1={input1} color2={input2} pct={pct}/>
